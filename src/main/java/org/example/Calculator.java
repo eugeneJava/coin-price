8f28612818410a3719a19769a9b547d3bf0c3388
Eugene Turovskiy <liquidnost@yahoo.com>
2026-06-13 20:54:28 +0300

    added tan

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -22,7 +22,7 @@ public class Calculator {
         return Math.abs(a);
     }
 
-    public double sin(double a) {
-        return Math.sin(a);
+    public double tan(double a) {
+        return Math.tan(a);
     }
 }
